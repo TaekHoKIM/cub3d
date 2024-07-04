516fabc34a9a032c4487051def1bac6f5f055479
minyekim <biie2004@naver.com>
2024-07-04 22:18:06 +0900

    Add : texteur를 열고, 정사각형 확인 후 xpm file을 image로 변환

---
+++ map_parsing/xpm_to_image.c
@@ -0,0 +1,70 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   xpm_to_image.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: minyekim <minyekim@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/04 21:19:00 by minyekim          #+#    #+#             */
+/*   Updated: 2024/07/04 22:16:41 by minyekim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "main.h"
+
+static void	xpm_to_image(void *mlx_ptr, t_img *img, char *file_name)
+{
+	img->img_ptr = mlx_xpm_file_to_image(mlx_ptr, file_name,
+		&(img->width), &(img->height));
+	if (img->img_ptr == NULL)
+		exit(EXIT_FAILURE);
+	img->data = mlx_get_data_addr(img->img_ptr,
+		&(img->bits_per_pixel), &(img->size_line),
+			&(img->endian));
+	if (img->data == NULL)
+		exit(EXIT_FAILURE);
+}
+
+static void	xpm_file_square_check(char *file_name, t_img *img)
+{
+	int		fd;
+	char	*line;
+	int		i;
+
+	fd = open(file_name, O_RDONLY);
+	if (fd == FAIL)
+		exit(EXIT_FAILURE);
+	img->width = -1;
+	img->height = 0;
+	while (1)
+	{
+		line = get_next_line(fd);
+		if (line == NULL)
+			break ;
+		i = 0;
+		while (line[i] != '\0')
+			i++;
+		if (img->width == -1)
+			img->width = i;
+		if (img->width != i)
+				exit(EXIT_FAILURE);
+		img->height++;
+	}
+	if (img->width != img->height)
+		exit(EXIT_FAILURE);
+}
+
+void	dir_xmp_file_to_image(void *mlx_ptr, t_map_info *info, t_img **img)
+{
+	*img = malloc(sizeof(t_img) * 4);
+	if (*img == NULL)
+		exit(EXIT_FAILURE);
+	xpm_file_square_check(info->no_filename, &((*img)[NO]));
+	xpm_to_image(mlx_ptr, &((*img)[NO]), info->no_filename);
+	xpm_file_square_check(info->no_filename, &((*img)[SO]));
+	xpm_to_image(mlx_ptr, &((*img)[SO]), info->so_filename);
+	xpm_file_square_check(info->no_filename, &((*img)[WE]));
+	xpm_to_image(mlx_ptr, &((*img)[WE]), info->we_filename);
+	xpm_file_square_check(info->no_filename, &((*img)[EA]));
+	xpm_to_image(mlx_ptr, &((*img)[EA]), info->ea_filename);
+}
